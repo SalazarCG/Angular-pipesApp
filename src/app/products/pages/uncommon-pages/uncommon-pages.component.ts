94cc1f43fa877d01f86fed7851fb09cd6b881ce5
Cristian <389336@alu.murciaeduca.es>
2025-01-13 11:01:07 +0100

    Hoja de Pipes no tan comunes. Se añaden i18nSelect, plural y slice

--- src/app/products/pages/uncommon-pages/uncommon-pages.component.ts
+++ src/app/products/pages/uncommon-pages/uncommon-pages.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-pages',
   templateUrl: './uncommon-pages.component.html',
-  styleUrl: './uncommon-pages.component.css'
+  styleUrl: './uncommon-pages.component.css',
 })
 export class UncommonPagesComponent {
+
+  //i18n Select
+  public name: string = 'Cristian';
+  public gender: 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Cloe';
+    this.gender = 'female';
+  }
+  // i18nPlural
+  public clients: string[] = ['Cristian', 'Cloe', 'Jhon', 'Doe', 'Jane'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': ' 1 cliente esperando',
+    '=2': ' 2 clientes esperando',
+    'other': ' # clientes esperando'
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
 
 }
